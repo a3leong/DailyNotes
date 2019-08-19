2c4d1d2df22fb48dde77cd0bf41927561e674cf0
a3leong <a3leong@gmail.com>
2019-08-19 02:07:12 -0700

    commit initial files

---
+++ src/apis/web-storage-apis.js
@@ -0,0 +1,6 @@
+/*
+ * API's to interact with local storage for browser
+ * Should be able to CRUD as well as handle the timestamp (we might be able to run a periodic or open check for a prompt)
+ *
+ *
+ */
